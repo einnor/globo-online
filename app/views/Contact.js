dd0876d7ea069b1419e605242a069dad9390d278
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-03-12 14:26:47 +0300

    Handling user input

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, Alert } from 'react-native';
+import { SatckNavigator } from 'react-navigation';
 
 import { Header } from '../sections/Header';
 
@@ -8,12 +9,41 @@ export class Contact extends Component {
     header: null,
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      msg: 'Enter Message',
+      name: 'Enter Name',
+      email: 'Enter your E-mail Address',
+    };
+  }
+
+  clearFileds = () => this.setState({ name: '', msg: '', email: '' });
+
+  sendMessage = () => {
+    const { navigation } = this.props;
+    const { name, msg } = this.state;
+    Alert.alert(name, msg);
+    navigation.goBack();
+  }
+
   render() {
+    const { name, msg, email } = this.state;
     return (
       <View style={styles.container}>
         <Header message="Press to Login" />
-        <Text style={{ flex: 8 }}>The contact form will go here</Text>
-        <Text style={{ flex: 6 }}>More contact info</Text>
+        <Text style={styles.heading}>The contact form will go here</Text>
+        
+        <TextInput style={styles.inputs} onChangeText={(text) => this.setState({ name: text })} value={name} />
+        <TextInput style={styles.multiInput} onChangeText={(text) => this.setState({ msg: text })} value={msg} multiline={true} numberOfLines={4} />
+        <TextInput style={styles.inputs} onChangeText={(text) => this.setState({ email: text })} value={email} />
+        <TouchableHighlight onPress={this.sendMessage} underlayColor="#31e981">
+          <Text style={styles.buttons}>Send Message</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.clearFileds} underlayColor="#31e981">
+          <Text style={styles.buttons}>Reset Form</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -22,5 +52,25 @@ export class Contact extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    paddingBottom: '45%',
+  },
+  heading: {
+    fontSize: 16,
+    flex: 1,
+  },
+  inputs: {
+    flex: 1,
+    width: '80%',
+    padding: 10,
+  },
+  multiInput: {
+    flex: 2,
+    width: '90%',
+    paddingTop: 20,
+  },
+  buttons: {
+    marginTop: 15,
+    fontSize: 16,
   }
 });
