ee1a1a06df21c87edc38c0eeffdea44dda65abd1
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-03-12 11:21:21 +0300

    Applying styles to a component

---
+++ app/sections/Header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  toggleUser = () => {
+    const { isLoggedIn } = this.state;
+    this.setState({ isLoggedIn: !isLoggedIn });
+  }
+
+  render() {
+    const { message } = this.props;
+    const { isLoggedIn } = this.state;
+    const display = isLoggedIn ? 'Sample User' : message
+    return (
+      <View style={styles.headStyle}>
+        <Text onPress={this.toggleUser} style={styles.headText}>{display}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  headText: {
+    textAlign: 'right',
+    color: '#ffffff',
+    fontSize: 20,
+  },
+  headStyle: {
+    paddingTop: 30,
+    paddingBottom: 10,
+    paddingRight: 10,
+    backgroundColor: '#35605a',
+  }
+});
--- app/views/Home.js
+++ app/views/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Header } from '../sections/Header';
+
 export class Home extends Component {
   render() {
     return (
       <View>
+        <Header message="Press to Login" />
         <Text>This will be the Home page</Text>
       </View>
     );
