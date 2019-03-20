c1cce9d43a06dac82ca3b1e1f75e94ed2e1db97e
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-03-20 10:14:13 +0300

    Added a blog component

---
+++ app/views/Blog.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Text, View, FlatList } from 'react-native';
+import HTML from 'react-native-render-html';
+
+export class Blog extends Component {
+  static navigationOptions = {
+    header: null,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      blogLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    return fetch('https://public-api.wordpress.com/rest/v1.1/sites/myglobomantics.wordpress.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ blogLoaded: true, blogList: Array.from(responseJson.posts) });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  chooseBlog = (blogID) => {}
+
+  render() {
+    const { blogLoaded, blogList } = this.state;
+    return (
+      <View>
+        {
+          blogLoaded && (
+            <View style={{ paddingTop: 40 }}>
+              <FlatList
+                data={blogList}
+                keyExtractor={(item, index) => item.ID.toString()}
+                renderItem={({ item }) => (
+                  <BlogItem
+                    id={item.id}
+                    title={item.title}
+                    imageSrc={item.featured_image}
+                    excerpt={item.excerpt}
+                    choosePost={this.chooseBlog}
+                  />
+                )}
+              />
+            </View>
+          )
+        }
+        {
+          !blogLoaded && (
+            <View style={{ paddingTop: 30 }}>
+              <Text>LOADING...</Text>
+            </View>
+          )
+        }
+      </View>
+    )
+  }
+}
+
+export class BlogItem extends Component {
+  blogChoice = () => {
+    const { choosePost, id } = this.props;
+    choosePost(id);
+  }
+  render() {
+    const { id, imageSrc, title, excerpt } = this.props;
+    const blogItems = `
+    <a href=${id} style="textDecorationLine: none; color: #000000; textAlign: center">
+      <img src=${imageSrc} />
+      <h1>${title}</h1>
+      ${excerpt}
+    </a>
+    `;
+    return (
+      <View style={{ borderBottomWidth: 2, borderBottomColor: '#000000', borderStyle: 'solid' }}>
+        <HTML html={blogItems} onLinkPress={() => this.blogChoice()}></HTML>
+      </View>
+    )
+  }
+}
+
