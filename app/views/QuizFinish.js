72f86e819ebf4824319368a3b6254c7077d3f91f
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-03-20 07:58:35 +0300

    Added a finish quiz component

---
+++ app/views/QuizFinish.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+
+export default class QuizFinish extends Component {
+  static navigationOptions = {
+    header: null,
+  }
+
+  exitQuiz = () => {
+    const { navigation } = this.props;
+    navigation.navigate('HomeRT');
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const userScore = navigation.getParam('score', 'Error - No score returned');
+    const questionsMissed = navigation.getParam('missed', 'Error - No missed questions');
+    const totalQuestions = navigation.getParam('questions', 'Error - No questions returned');
+
+    return (
+      <View style={styles.container}>
+        <Text>Your quiz score was {userScore}</Text>
+        <Text>You missed on {questionsMissed} out of {totalQuestions}</Text>
+
+        <TouchableHighlight onPress={this.exitQuiz} style={styles.button}>
+          <Text>Finish Quiz</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+  },
+});
